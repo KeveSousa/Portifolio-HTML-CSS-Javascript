aa17dc16a7f0875dba993536a3bd8664f6545615
KeveSousa <104788674+KeveSousa@users.noreply.github.com>
2022-11-01 17:39:39 +0000

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+let toggle = document.querySelector('.toggle');
+let topbar = document.querySelector('.topbar');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    topbar.classList.toggle('active');
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+    
+    
+} //transição da pagina quando for clicado no butão de alternar.
